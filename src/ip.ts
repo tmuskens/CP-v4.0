654cf65eca87dc2456b1ad1c0d067bfdc4c1a584
Trent Muskens <tmuskens@andrew.cmu.edu>
2022-03-10 10:45:53 -0500

    fixed ip situation

--- src/ip.ts
+++ src/ip.ts
@@ -3,9 +3,18 @@ import { networkInterfaces } from 'os'
 const nets: any = networkInterfaces()
 
 export function getIp (): string {
-  for (const x of nets.en1) {
-    if (x.family === 'IPv4') {
-      return x.address
+  if (nets.en1 !== undefined) {
+    for (const x of nets.en1) {
+      if (x.family === 'IPv4') {
+        return x.address
+      }
+    }
+  }
+  if (nets.en0 !== undefined) {
+    for (const x of nets.en0) {
+      if (x.family === 'IPv4') {
+        return x.address
+      }
     }
   }
   throw new Error('ip not found')
